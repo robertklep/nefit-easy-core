2b27327c4a5c07877327a711fa284e10850946db
Robert Klep <robert@klep.name>
2018-08-11 12:28:25 +0200

    Converted to class

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-// Patch `crypto` so `tls-connect` doesn't trigger a deprecation warning.
-require('crypto').__defineGetter__('createCredentials', () => require('tls').createSecureContext);
-
-// Patch `tls` so `node-xmpp-tls-connect` doesn't trigger a deprecation warning.
-require('tls').convertNPNProtocols = null;
-
 console.log('[old-client-with-scram]');
 module.exports = require('./lib');
